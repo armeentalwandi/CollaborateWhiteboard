a6483486eb3a634f23dcb93b5d79aede030e5833
Harsh Dave <hdave009@gmail.com>
2023-10-11 14:29:41 -0400

    Added button icons, columns

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,19 +9,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import composables.TwoColumnLayout
 
 @Composable
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
+    TwoColumnLayout()
 }
 
 fun main() = application {
---
+++ src/main/kotlin/composables/TwoColumns.kt
@@ -0,0 +1,116 @@
+package composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.R
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TwoColumnLayout() {
+    MaterialTheme {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            // Left Column
+            var drawButtonState by remember { mutableStateOf(true) }
+            var eraseButtonState by remember { mutableStateOf(false) }
+            var selectButtonState by remember { mutableStateOf(false) }
+            var drawShapeButtonState by remember { mutableStateOf(false) }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Cyan)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                TextButton(
+                    onClick = {
+                        drawButtonState = true
+                        eraseButtonState = false
+                        selectButtonState = false
+                        drawShapeButtonState = false
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(8.dp).background(if (drawButtonState) Color.LightGray else Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource("pen.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.background(Color.Transparent)
+                    )
+                }
+
+                TextButton(
+                    onClick = {
+                        drawButtonState = false
+                        eraseButtonState = true
+                        selectButtonState = false
+                        drawShapeButtonState = false
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(8.dp).background(if (eraseButtonState) Color.LightGray else Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource("eraser.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.background(Color.Transparent)
+                    )
+                }
+
+                TextButton(
+                    onClick = {
+                        drawButtonState = false
+                        eraseButtonState = false
+                        selectButtonState = true
+                        drawShapeButtonState = false
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(8.dp).background(if (selectButtonState) Color.LightGray else Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource("select.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.background(Color.Transparent)
+                    )
+                }
+
+                TextButton(
+                    onClick = {
+                        drawButtonState = false
+                        eraseButtonState = false
+                        selectButtonState = false
+                        drawShapeButtonState = true
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(8.dp).background(if (drawShapeButtonState) Color.LightGray else Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource("shapes.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.background(Color.Transparent)
+                    )
+                }
+            }
+
+            // Right Column
+            Column(
+                modifier = Modifier
+                    .weight(4f)
+                    .fillMaxHeight()
+                    .background(Color.White)
+                    .padding(16.dp)
+                    .background(Color.Magenta)
+            ) {
+                // Canvas or right column content here
+
+            }
+        }
+    }
+}
