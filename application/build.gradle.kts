9e5f3455768399b8bd76ea0de2bb7a0339cac527
Armeen Talwandi <armeentalwandi@gmail.com>
2023-11-03 14:38:23 -0400

    updated release version

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -37,7 +37,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "theappengers"
-            packageVersion = "1.0.0"
+            packageVersion = "1.1.0"
         }
     }
 }
