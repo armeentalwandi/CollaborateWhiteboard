ffdc36d816e99e47b094d8ca76ecf06dea6a1c4b
Armeen Talwandi <armeentalwandi@gmail.com>
2023-11-27 21:05:31 -0500

    stroke testing finished

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
